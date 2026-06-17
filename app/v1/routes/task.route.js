44b04fe47351dd37e8998504f78a8f71eb1b37c3
Tanpaisen <tinh7demtb@gmail.com>
2026-06-17 13:35:35 +0700

    Cập nhật trạng thái nhiều sản phẩm

--- app/v1/routes/task.route.js
+++ app/v1/routes/task.route.js
@@ -9,5 +9,8 @@ router.get('/detail/:id', controller.detail);
 
 router.patch('/change-status/:id', controller.changeStatus);
 
+router.patch('/change-multi', controller.changeMulti);
+
+
 
 module.exports = router;
